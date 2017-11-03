8fc1c6d78319fbd950a879b9c0361c5678f8115a
Praveen Kumar <prvnbeloved@gmail.com>
2017-11-03 10:45:16 +0530

    C++/Introduction Problems Complete

---
+++ C++/Introduction/Input and Output.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int A,B,C;
+    cin>>A>>B>>C;
+    cout<<A+B+C<<endl;
+    return 0;
+}
